a0c765b7e211d712a27985a1b8bf2b175daeca59
bbrancar <brydonpbrancart@gmail.com>
2022-07-12 11:44:36 -0700

    Update custom_network.py

--- custom_network.py
+++ custom_network.py
@@ -180,84 +180,74 @@ class Res_Unet(nn.Module):
 
     def forward(self, x):
         #32,3,384,768
-        #print('x', x.shape)
         x1 = self.conv_block_1(x) + self.conv_skip_1(x)
-        #print('x1', x1.shape)
+
         #32, 32, 192, 384
         x2 = self.conv_block_2(x1) + self.conv_skip_2(x1)
-        #print('x2', x2.shape)
+
         #32, 64, 96, 192
         x3 = self.conv_block_3(x2) + self.conv_skip_3(x2)
-        #print('x3', x3.shape)
+
         #32, 128, 48, 96
         x4 = self.conv_block_4(x3) + self.conv_skip_4(x3)
-        #print('x4', x4.shape)
+
         #32, 256, 24, 48
         x5 = self.conv_block_5(x4) + self.conv_skip_5(x4)
-        #print('x5', x5.shape)
+
         #32, 512, 12, 24
         x6 = self.conv_block_6(x5) + self.conv_skip_6(x5)
-        #print('x6', x6.shape)
+
         #32, 1024, 6, 12
         x6 = self.deconv0(x6)
+      
         #32, 1024, 12, 24
-        #print('x6 upsampled', x6.shape)
         x7 = torch.cat([x6, x5], dim = 1)
-        #32, 1536, 12, 24
-        #print('x7', x7.shape)
+      
         #32, 512, 12, 24
         x8 = self.deconv0_res(x7) + self.deconv_skip_0(x7)
+      
         #32, 512, 12, 24
-        #print('x8 ', x8.shape)
-        
         x8 = self.deconv1(x8)
+      
         #32, 512, 24, 48
-        #print('x8_upsample', x8.shape)
-
         x9 = torch.cat([x8,x4], dim = 1)
+      
         #32, 768, 24, 48
-        #print('x9', x9.shape)
         x10 = self.deconv1_res(x9) + self.deconv_skip_1(x9)
+      
         #32, 256, 24, 48
-        #print('x10', x10.shape)
         x10 = self.deconv2(x10)  
+      
         #32, 256, 48, 96
-        #print('x10 upsample', x10.shape)
         x11 = torch.cat([x10,x3], dim = 1)
+      
         #32, 384, 48, 96
-        #print('x11', x11.shape)
         x12 = self.deconv2_res(x11) + self.deconv_skip_2(x11)
+      
         #32, 128, 48, 96
-        #print('x12', x12.shape)
-        
-        
-        
         x12 = self.deconv3(x12)
+      
         #32, 128, 96, 192
-        #print('x12 upsample', x12.shape)
         x13 = torch.cat([x12,x2], dim = 1)
+      
         #32, 192, 96, 192
-        #print('x13', x13.shape)
         x14 = self.deconv3_res(x13) + self.deconv_skip_3(x13)
+      
         #32, 64, 96, 192
-        #print('x14', x14.shape)
-        
         x14 = self.deconv4(x14)
+      
         #32, 64, 192, 384
-        #print('x14 upsample', x14.shape)
         x15 = torch.cat([x14,x1], dim = 1)
+      
         #32, 96, 192, 384
-        #print('x15', x15.shape)
         x16 = self.deconv4_res(x15) + self.deconv_skip_4(x15)
+      
         #32, 32, 192, 384
-        #print('x16', x16.shape)
-        
         x16 = self.deconv5(x16)
-        #print('x16 upsample', x16.shape)
+
         #32, 32, 384, 768
         score = self.classifier(x16)                   
-        #print('score',score.shape)
-        #32, 10, 384, 768
+
         return score  # size=(N, n_class, x.H/1, x.W/1)
     
 if __name__ == '__main__':
